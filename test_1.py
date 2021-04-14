456e1dc937778c5e3b1784531f174443a5bcbf5c
gerich <yl.herasimau@gmail.com>
2021-04-14 16:17:09 +0300

    add task results for Andersen

---
+++ test_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# 1. Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+
+import os
+number = float(input('Введите число: '))
+
+if number > 7:
+    print('Привет')
+
+os.system("pause")
